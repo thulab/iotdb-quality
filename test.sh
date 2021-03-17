90e279b25a1d46d293cb12c771131744ea42b438
trin1t <48903014+trin1t@users.noreply.github.com>
2021-03-17 21:09:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,13 +5,6 @@ docker run --name iotdb-quality-cpy -d iotdb-quality:cpy /bin/bash
 # test scripts here
 echo Test started.
 docker exec iotdb-quality-cpy /bin/sh -c "mv /udf-tsclean-0.1.0-jar-with-dependencies.jar /iotdb/ext/udf/"
-docker exec iotdb-quality-cpy /bin/bash start-cli.sh“create function completeness as 'cn.edu.thu.dquality.udf.UDTFCompleteness'”
-docker exec iotdb-quality-cpy /bin/bash “create function completeness as 'cn.edu.thu.dquality.udf.UDTFCompleteness'”
-#create function consistency as 'cn.edu.thu.dquality.udf.UDTFConsistency'
-#create function timeliness as 'cn.edu.thu.dquality.udf.UDTFTimeliness'
-#create function validity as 'cn.edu.thu.dquality.udf.UDTFValidity'
-#create function percentile as 'cn.edu.thu.dquality.udf.UDAFPercentile'
-exit
 echo Test finished.
 # delete docker image & container.
 docker stop iotdb-quality-cpy
