bf21be1b96639ad7485f03e91daa2dd9ffd61847
trin1t <48903014+trin1t@users.noreply.github.com>
2021-03-24 14:28:02 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # delete last docker image & container.
-docker stop iotdb-quality-cpy
-docker rm iotdb-quality-cpy
-docker rmi iotdb-quality:cpy
+docker stop iotdb-quality-master
+docker rm iotdb-quality-master
+docker rmi iotdb-quality:master
 echo Docker image deleted.
 # create docker image & container
-docker build -t iotdb-quality:cpy -f Dockerfile .
-docker run --name iotdb-quality-cpy -d -p 6667:6667 -p 31999:31999 -p 8181:8181 -p 5555:5555 iotdb-quality:cpy /bin/bash
+docker build -t iotdb-quality:master -f Dockerfile .
+docker run --name iotdb-quality-master -d -p 6667:6667 -p 31999:31999 -p 8181:8181 -p 5555:5555 iotdb-quality:cpy /bin/bash
 # test scripts here
 echo Test started.
 docker exec iotdb-quality-cpy /bin/sh -c "mv /iotdb-quality-0.1.0-jar-with-dependencies.jar /iotdb/ext/udf/"
@@ -15,7 +15,7 @@ cd /media/sf_sharedfolder/iotdb-benchmark-dev_quality_ci/bin/
 sh startup.sh
 echo Test finished.
 # delete docker image & container.
-docker stop iotdb-quality-cpy
-docker rm iotdb-quality-cpy
-docker rmi iotdb-quality:cpy
+docker stop iotdb-quality-master
+docker rm iotdb-quality-master
+docker rmi iotdb-quality:master
 echo Docker image deleted.
