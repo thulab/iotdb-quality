439344ba7df73fc92628bb3ff6c5dfe4a262c98a
SiasDoming <1130063240@qq.com>
2021-04-02 15:08:57 +0800

    add UDF Integral

--- docs/en/DataProfiling/Integral.md
+++ docs/en/DataProfiling/Integral.md
@@ -1,1 +1,82 @@
 # Integral
+
+## Usage
+
+This function is used to calculate the integration of time series, 
+which equals to the area under the curve with time as X-axis and values as Y-axis.
+
+**Name:** INTEGRAL
+
+**Input Series:** Only support a single input numeric series. The type is INT32 / INT64 / FLOAT / DOUBLE.
+
+**Parameters:**
+
++ `unit`: The unit of time used when computing the integral. 
+  The value should be chosen from "1S", "1s", "1m", "1H", "1d"(case-sensitive), 
+  and each represents taking one millisecond / second / minute / hour / day as 1.0 while calculating the area and integral.
+
+**Output Series:** Output a single series. The type is DOUBLE.
+
+**Note:**
+
++ The integral value equals to the sum of the areas of right-angled trapezoids consisting of each two adjacent points and the time-axis. 
+Choosing different `unit` implies different scaling of time axis, thus making it apparent to convert the value among those results with constant coefficient.
+  
++ The calculation ignores missing points and null values. The curve or trapezoids will skip these points and use the next valid point.
+
+## Examples
+
+Input series:
+```
++-----------------------------+---------------+
+|                         Time|root.test.d1.s1|
+|2020-01-01T00:00:01.000+08:00|              1|
+|2020-01-01T00:00:02.000+08:00|              2|
+|2020-01-01T00:00:03.000+08:00|              3|
+|2020-01-01T00:00:03.000+08:00|              4|
+|2020-01-01T00:00:03.000+08:00|              5|
+|2020-01-01T00:00:04.000+08:00|              6|
+|2020-01-01T00:00:05.000+08:00|              7|
+|2020-01-01T00:00:08.000+08:00|              8|
+|2020-01-01T00:00:09.000+08:00|            NaN|
+|2020-01-01T00:00:10.000+08:00|             10|
+```
+
+### Default Parameters
+
+With default parameters, this function will take one second as 1.0. SQL for query:
+
+```sql
+select integral(s1) from root.test.d1 where time <= 2020-01-01 00:00:10
+```
+
+Output series:
+```
++-----------------------------+-------------------------+
+|                         Time|integral(root.test.d1.s1)|
++-----------------------------+-------------------------+
+|2020-01-01T00:00:10.000+08:00|                     56.5|
++-----------------------------+-------------------------+
+```
+
+Calculation expression: (1+2)/2.0*1 + (2+3)/2.0*1 + (5+6)/2.0*1 + (6+7)/2.0*1 + (7+8)/2.0*3 + (8+10)/2.0*2 = 56.5
+
+### Specific time unit
+
+With time unit specified as "1m", this function will take one minute as 1.0. SQL for query:
+
+```sql
+select integral(s1, "unit"="1m") from root.test.d1 where time <= 2020-01-01 00:00:10
+```
+
+Output series:
+```
++-----------------------------+-------------------------+
+|                         Time|integral(root.test.d1.s1)|
++-----------------------------+-------------------------+
+|2020-01-01T00:00:10.000+08:00|                    0.942|
++-----------------------------+-------------------------+
+```
+
+Calculation expression: (1+2)/2.0*(1.0/60) + (2+3)/2.0*(1.0/60) + (5+6)/2.0*(1.0/60) + (6+7)/2.0*(1.0/60) + (7+8)/2.0*(3.0/60) + (8+10)/2.0*(2.0/60) = 0.941667
+
---
+++ src/main/java/cn/edu/thu/iotdb/quality/dprofile/UDTFIntegral.java
@@ -0,0 +1,108 @@
+package cn.edu.thu.iotdb.quality.dprofile;
+
+import cn.edu.thu.iotdb.quality.NoNumberException;
+import org.apache.iotdb.db.query.udf.api.UDTF;
+import org.apache.iotdb.db.query.udf.api.access.Row;
+import org.apache.iotdb.db.query.udf.api.collector.PointCollector;
+import org.apache.iotdb.db.query.udf.api.customizer.config.UDTFConfigurations;
+import org.apache.iotdb.db.query.udf.api.customizer.parameter.UDFParameters;
+import org.apache.iotdb.db.query.udf.api.customizer.strategy.RowByRowAccessStrategy;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+
+/**
+ * calculate the integral or the area under the curve of input series
+ * $unit$ is the time scale for the area calculation, chosen from 1s(second, default), 1m(minute), 1h(hour), 1d(day)
+ * @register: CREATE FUNCTION INTEGRAL AS "cn.edu.thu.iotdb.quality.dprofile.UDTFIntegral"
+ * @usage: SELECT INTEGRAL(s0, "unit"="1s") FROM root.test
+ */
+public class UDTFIntegral implements UDTF {
+
+    private static final String TIME_UNIT_KEY="unit";
+    private static final String TIME_UNIT_MS="1S";
+    private static final String TIME_UNIT_S="1s";
+    private static final String TIME_UNIT_M="1m";
+    private static final String TIME_UNIT_H="1H";
+    private static final String TIME_UNIT_D="1d";
+
+    long unitTime;
+    Object lastTime;
+    Object lastValue;
+    double integralValue;
+
+    @Override
+    public void beforeStart(UDFParameters udfParameters, UDTFConfigurations udtfConfigurations) throws Exception {
+        udtfConfigurations.setAccessStrategy(new RowByRowAccessStrategy());
+        switch (udfParameters.getDataType(0)) {
+            case INT32:
+            case INT64:
+            case FLOAT:
+            case DOUBLE:
+                switch (udfParameters.getStringOrDefault(TIME_UNIT_KEY, TIME_UNIT_S).toLowerCase()) {
+                    case TIME_UNIT_MS:
+                        unitTime = 1;
+                    case TIME_UNIT_S:
+                        unitTime = 1000;
+                        break;
+                    case TIME_UNIT_M:
+                        unitTime = 60000;
+                        break;
+                    case TIME_UNIT_H:
+                        unitTime = 3600000;
+                        break;
+                    case TIME_UNIT_D:
+                        unitTime = 3600000*24;
+                        break;
+                    default:
+                        throw new Exception("Unknown time unit input: "+udfParameters.getStringOrDefault(TIME_UNIT_KEY, TIME_UNIT_S).toLowerCase());
+                }
+                udtfConfigurations.setOutputDataType(TSDataType.DOUBLE);
+                lastTime = null;
+                lastValue = null;
+                integralValue = 0D;
+                break;
+            case BOOLEAN:
+            case TEXT:
+                throw new NoNumberException();
+        }
+    }
+
+    @Override
+    public void transform(Row row, PointCollector collector) throws Exception {
+        if (!row.isNull(0)) {
+
+            long nowTime = row.getTime();
+            double nowValue;
+            switch (row.getDataType(0)) {
+                case INT32:
+                    nowValue = row.getInt(0);
+                    break;
+                case INT64:
+                    nowValue = row.getLong(0);
+                    break;
+                case FLOAT:
+                    nowValue = row.getFloat(0);
+                    break;
+                case DOUBLE:
+                    nowValue = row.getDouble(0);
+                    break;
+                case BOOLEAN:
+                case TEXT:
+                default:
+                    throw new NoNumberException();
+            }
+            // calculate the ladder-shaped area between last point and this one
+            // skip and initialize the memory if no existing previous point is available
+            if (lastValue != null) {
+                integralValue += ((double) lastValue + nowValue) * (nowTime - (long) lastTime) / 2.0 / unitTime;
+            }
+            lastTime = nowTime;
+            lastValue = nowValue;
+        }
+    }
+
+    @Override
+    public void terminate(PointCollector collector) throws Exception {
+        collector.putDouble((long) lastTime, integralValue);
+    }
+
+}
