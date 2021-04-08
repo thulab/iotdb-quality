11e375503d844f7dba5729f81a6fbf7963a39bd6
unknown <543202718@qq.com>
2021-04-08 16:30:25 +0800

    modify site building scripts

--- tools/site/deploy.sh
+++ tools/site/deploy.sh
@@ -1,12 +1,22 @@
 # Clean docs/
 rm -rf docs/*
-# Copy documents to docs/
+# Copy documents of master to docs/
 cp ../../README.md docs/
 cp -r ../../docs/* docs/
-mkdir docs/V1.0.x
-mkdir docs/zh/V1.0.x
-cp -r V1.0.x/en/* docs/V1.0.x
-cp -r V1.0.x/zh/* docs/zh/V1.0.x
+# Copy documents of V1.0.x to docs/
+cd ..
+cd ..
+git checkout v1.0.x
+mkdir ../tmp
+cp -r docs/* ../tmp
+cp README.md ../tmp/en
+git checkout master
+mkdir tools/site/docs/V1.0.x
+mkdir tools/site/docs/zh/V1.0.x
+cp -r ../tmp/en/* tools/site/docs/V1.0.x
+cp -r ../tmp/zh/* tools/site/docs/zh/V1.0.x
+rm -rf ../tmp
+cd tools/site
 # Build the web pages
 npm run docs:build
 # Remove the copied documents
