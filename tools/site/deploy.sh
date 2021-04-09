5cd13f8a3ee9da6de4730e9cb0a1860b33bd3dec
unknown <543202718@qq.com>
2021-04-09 10:23:27 +0800

    modify deploy.sh

--- tools/site/deploy.sh
+++ tools/site/deploy.sh
@@ -3,18 +3,22 @@ rm -rf docs/*
 # Copy documents of master to docs/
 cp ../../README.md docs/
 cp -r ../../docs/* docs/
-# Copy documents of V1.0.x to docs/
+# Copy documents and downloadable files of V1.0.x to docs/
 cd ..
 cd ..
 git checkout v1.0.x
 mkdir ../tmp
+mkdir ../tmp/download
 cp -r docs/* ../tmp
 cp README.md ../tmp/en
+cp -r download/* ../tmp/download
 git checkout master
 mkdir tools/site/docs/V1.0.x
 mkdir tools/site/docs/zh/V1.0.x
+mkdir download/V1.0.x
 cp -r ../tmp/en/* tools/site/docs/V1.0.x
 cp -r ../tmp/zh/* tools/site/docs/zh/V1.0.x
+cp -r ../tmp/download/* download/V1.0.x
 rm -rf ../tmp
 cd tools/site
 # Build the web pages
