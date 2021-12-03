929ba18cbdaaea937f927d745b12b8f0b88737c8
trin1t <48903014+trin1t@users.noreply.github.com>
2021-12-03 15:04:46 +0800

    Update Util.java

--- src/main/java/org/apache/iotdb/quality/util/Util.java
+++ src/main/java/org/apache/iotdb/quality/util/Util.java
@@ -31,16 +31,20 @@ import org.eclipse.collections.impl.map.mutable.primitive.LongIntHashMap;
 
 import java.util.ArrayList;
 
-/** @author Wang Haoyu */
+/** @author Wang Haoyu
+ *  This class offers functions of getting and putting values from iotdb interface.
+ */
 public class Util {
 
   /**
    * 从Row中取出指定位置的值，并转化为double类型。注意，必须保证Row中不存在null。
+   * Get value from specific column from Row, and cast to double.
+   * Make sure never get null from Row.
    *
-   * @param row 数据行
-   * @param index 指定位置的索引
-   * @return Row中的指定位置的值
-   * @throws NoNumberException Row中的指定位置的值是非数值类型
+   * @param row 数据行 data row
+   * @param index 指定位置的索引 the column index
+   * @return Row中的指定位置的值 value of specific column from Row
+   * @throws NoNumberException Row中的指定位置的值是非数值类型 when getting a no number datatype
    */
   public static double getValueAsDouble(Row row, int index) throws NoNumberException {
     double ans = 0;
@@ -65,10 +69,12 @@ public class Util {
 
   /**
    * 从Row中取出第0列的值，并转化为double类型。注意，必须保证Row中不存在null。
+   * Get value from 0th column from Row, and cast to double.
+   * Make sure never get null from Row.
    *
-   * @param row 数据行
-   * @return Row中的第0列的值
-   * @throws NoNumberException Row中的第0列的值是非数值类型
+   * @param row 数据行 data row
+   * @return Row中的第0列的值 value from 0th column from Row
+   * @throws NoNumberException Row中的第0列的值是非数值类型 Row中的指定位置的值是非数值类型 when getting a no number datatype
    */
   public static double getValueAsDouble(Row row) throws NoNumberException {
     return getValueAsDouble(row, 0);
@@ -76,9 +82,10 @@ public class Util {
 
   /**
    * 从Row中取出第一个值，并转化为Object类型
+   * Get value from 0th column from Row, and cast to Object.
    *
    * @param row
-   * @return Row中的第一个值
+   * @return Row中的第一个值 value from 0th column from Row
    */
   public static Object getValueAsObject(Row row) {
     Object ans = 0;
@@ -107,11 +114,12 @@ public class Util {
 
   /**
    * 向PointCollector中加入新的数据点
+   * Add new data point to PointCollector
    *
    * @param pc PointCollector
-   * @param type 数据类型
-   * @param t 时间戳
-   * @param o Object类型的值
+   * @param type 数据类型 datatype
+   * @param t 时间戳 timestamp
+   * @param o Object类型的值 value in Object type
    * @throws Exception
    */
   public static void putValue(PointCollector pc, TSDataType type, long t, Object o)
@@ -138,45 +146,35 @@ public class Util {
   }
 
   /**
-   * 将{@code ArrayList<Double>}转化为长度相同的{@code double[]}。
+   * cast {@code ArrayList<Double>} to {@code double[]}
    *
-   * <p>用户需要保证{@code ArrayList<Double>}中没有空值{@code null}
-   *
-   * @param list 待转化的{@code ArrayList<Double>}
-   * @return 转化后的{@code double[]}
+   * @param list ArrayList to cast
+   * @return cast result
    */
   public static double[] toDoubleArray(ArrayList<Double> list) {
-    int len = list.size();
-    double ans[] = new double[len];
-    for (int i = 0; i < len; i++) {
-      ans[i] = list.get(i);
-    }
-    return ans;
+    return list.stream().mapToDouble(Double::valueOf).toArray();
   }
 
   /**
-   * 将{@code ArrayList<Long>}转化为长度相同的{@code long[]}。
+   * cast {@code ArrayList<Long>} to {@code long[]}
    *
-   * <p>用户需要保证{@code ArrayList<Long>}中没有空值{@code null}
-   *
-   * @param list 待转化的{@code ArrayList<Long>}
-   * @return 转化后的{@code long[]}
+   * @param list ArrayList to cast
+   * @return cast result
    */
   public static long[] toLongArray(ArrayList<Long> list) {
-    int len = list.size();
-    long ans[] = new long[len];
-    for (int i = 0; i < len; i++) {
-      ans[i] = list.get(i);
-    }
-    return ans;
+    return list.stream().mapToLong(Long::valueOf).toArray();
   }
 
   /**
    * 计算序列的绝对中位差MAD。为了达到渐进正态性，乘上比例因子1.4826。 <br>
    * 备注: 1.4826 = 1/qnorm(3/4)
    *
-   * @param value 序列
-   * @return 绝对中位差MAD
+   * calculate median absolute deviation of input series.
+   * 1.4826 is multiplied in order to achieve asymptotic normality.
+   * Note: 1.4826 = 1/qnorm(3/4)
+   *
+   * @param value 序列 input series
+   * @return 绝对中位差MAD median absolute deviation MAD
    */
   public static double mad(double[] value) {
     Median median = new Median();
@@ -190,9 +188,10 @@ public class Util {
 
   /**
    * 计算序列的取值变化
+   * calculate 1-order difference of input series
    *
-   * @param origin 原始序列
-   * @return 取值变化序列
+   * @param origin 原始序列 original series
+   * @return 取值变化序列 1-order difference
    */
   public static double[] variation(double origin[]) {
     int n = origin.length;
@@ -205,9 +204,10 @@ public class Util {
 
   /**
    * 计算序列的取值变化
+   * calculate 1-order difference of input series
    *
-   * @param origin 原始序列
-   * @return 取值变化序列
+   * @param origin 原始序列 original series
+   * @return 取值变化序列 1-order difference
    */
   public static double[] variation(long origin[]) {
     int n = origin.length;
@@ -220,9 +220,10 @@ public class Util {
 
   /**
    * 计算序列的取值变化
+   * calculate 1-order difference of input series
    *
-   * @param origin 原始序列
-   * @return 取值变化序列
+   * @param origin 原始序列 original series
+   * @return 取值变化序列 1-order difference
    */
   public static int[] variation(int origin[]) {
     int n = origin.length;
@@ -235,10 +236,11 @@ public class Util {
 
   /**
    * 计算时间序列的速度
+   * calculate speed (1-order derivative with backward difference)
    *
-   * @param origin 值序列
-   * @param time 时间戳序列
-   * @return 速度序列
+   * @param origin 值序列 value series
+   * @param time 时间戳序列 timestamp series
+   * @return 速度序列 speed series
    */
   public static double[] speed(double origin[], double time[]) {
     int n = origin.length;
@@ -251,10 +253,11 @@ public class Util {
 
   /**
    * 计算时间序列的速度
+   * calculate speed (1-order derivative with backward difference)
    *
-   * @param origin 值序列
-   * @param time 时间戳序列
-   * @return 速度序列
+   * @param origin 值序列 value series
+   * @param time 时间戳序列 timestamp series
+   * @return 速度序列 speed series
    */
   public static double[] speed(double origin[], long time[]) {
     int n = origin.length;
@@ -267,9 +270,10 @@ public class Util {
 
   /**
    * 计算序列的众数
+   * computes mode
    *
-   * @param values 序列
-   * @return 众数
+   * @param values 序列 input series
+   * @return 众数 mode
    */
   public static long mode(long[] values) {
     LongIntHashMap map = new LongIntHashMap();
@@ -287,6 +291,11 @@ public class Util {
     return key;
   }
 
+  /**
+   * cast String to timestamp
+   * @param s input string
+   * @return timestamp
+   */
   public static long parseTime(String s) {
     long unit = 0;
     s = s.toLowerCase();
