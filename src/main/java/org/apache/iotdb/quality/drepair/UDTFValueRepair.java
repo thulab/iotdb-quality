f8316c2684362e7a6e981a383033d9283a667ab0
trin1t <48903014+trin1t@users.noreply.github.com>
2021-12-17 15:38:45 +0800

    new tests

--- src/main/java/org/apache/iotdb/quality/drepair/UDTFValueRepair.java
+++ src/main/java/org/apache/iotdb/quality/drepair/UDTFValueRepair.java
@@ -31,7 +31,6 @@ import org.apache.iotdb.db.query.udf.api.customizer.strategy.SlidingSizeWindowAc
 import org.apache.iotdb.quality.drepair.util.LsGreedy;
 import org.apache.iotdb.quality.drepair.util.Screen;
 import org.apache.iotdb.quality.drepair.util.ValueRepair;
-import org.apache.iotdb.quality.util.Util;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 
 /** This function is used to repair the value of the time series. */
@@ -48,14 +47,15 @@ public class UDTFValueRepair implements UDTF {
         .validateInputSeriesNumber(1)
         .validateInputSeriesDataType(
             0, TSDataType.FLOAT, TSDataType.DOUBLE, TSDataType.INT32, TSDataType.INT64)
-            .validate(x -> (double) x > 0,
-                    "Parameter $sigma$ should be larger than 0.",
-                    validator.getParameters().getDoubleOrDefault("sigma",1.0))
-            .validate(
-                    params -> (double) params[0] < (double) params[1],
-                    "parameter $minSpeed$ should be smaller than $maxSpeed$.",
-                    validator.getParameters().getDoubleOrDefault("minSpeed", -1),
-                    validator.getParameters().getDoubleOrDefault("maxSpeed", 1));
+        .validate(
+            x -> (double) x > 0,
+            "Parameter $sigma$ should be larger than 0.",
+            validator.getParameters().getDoubleOrDefault("sigma", 1.0))
+        .validate(
+            params -> (double) params[0] < (double) params[1],
+            "parameter $minSpeed$ should be smaller than $maxSpeed$.",
+            validator.getParameters().getDoubleOrDefault("minSpeed", -1),
+            validator.getParameters().getDoubleOrDefault("maxSpeed", 1));
   }
 
   @Override
