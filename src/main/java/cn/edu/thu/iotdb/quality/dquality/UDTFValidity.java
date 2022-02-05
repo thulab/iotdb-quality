3ad6fd408fa2ab77a4df5bca8835a87415552321
yikungong <501344806@qq.com>
2022-02-05 16:25:04 +0800

    update

--- src/main/java/cn/edu/thu/iotdb/quality/dquality/UDTFValidity.java
+++ src/main/java/cn/edu/thu/iotdb/quality/dquality/UDTFValidity.java
@@ -11,13 +11,16 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.iotdb.db.query.udf.api.UDTF;
+import org.apache.iotdb.db.query.udf.api.access.Row;
 import org.apache.iotdb.db.query.udf.api.access.RowWindow;
 import org.apache.iotdb.db.query.udf.api.collector.PointCollector;
 import org.apache.iotdb.db.query.udf.api.customizer.config.UDTFConfigurations;
 import org.apache.iotdb.db.query.udf.api.customizer.parameter.UDFParameters;
+import org.apache.iotdb.db.query.udf.api.customizer.strategy.RowByRowAccessStrategy;
 import org.apache.iotdb.db.query.udf.api.customizer.strategy.SlidingSizeWindowAccessStrategy;
 import org.apache.iotdb.db.query.udf.api.customizer.strategy.SlidingTimeWindowAccessStrategy;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import cn.edu.thu.iotdb.quality.dquality.ValidityComputing;
 
 /**
  * 用于计算时间序列的有效性的UDTF
@@ -26,38 +29,38 @@ import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
  */
 public class UDTFValidity implements UDTF {
 
+    boolean usePreSpeed = false;
+    double sMin;
+    double sMax;
+    ValidityComputing tsq = new ValidityComputing();
+
+    public UDTFValidity() throws Exception {
+    }
+
+
     @Override
-    public void beforeStart(UDFParameters udfp, UDTFConfigurations udtfc) throws Exception {
-        boolean isTime = false;
-        long window = Integer.MAX_VALUE;
-        if (udfp.hasAttribute("window")) {
-            String s = udfp.getString("window");
-            window = Util.parseTime(s);
-            if (window > 0) {
-                isTime = true;
-            } else {
-                window = Long.parseLong(s);
-            }
-        }
-        if (isTime){
-            udtfc.setAccessStrategy(new SlidingTimeWindowAccessStrategy(window));
-        }else{
-            udtfc.setAccessStrategy(new SlidingSizeWindowAccessStrategy((int) window));
-        }       
-        udtfc.setOutputDataType(TSDataType.DOUBLE);
+    public void beforeStart(UDFParameters udfParameters, UDTFConfigurations udtfConfigurations) throws Exception {
+        usePreSpeed = udfParameters.getBooleanOrDefault("usePreSpeed", false);
+        sMax = udfParameters.getDoubleOrDefault("sMax", Double.MAX_VALUE);
+        sMin = udfParameters.getDoubleOrDefault("sMin", -Double.MAX_VALUE + 1);
+        udtfConfigurations.setAccessStrategy(new RowByRowAccessStrategy())
+                .setOutputDataType(TSDataType.DOUBLE);
     }
 
     @Override
-    public void transform(RowWindow rowWindow, PointCollector collector) throws Exception {
-        try {
-            if (rowWindow.windowSize() > TimeSeriesQuality.WINDOWSIZE) {
-                TimeSeriesQuality tsq = new TimeSeriesQuality(rowWindow.getRowIterator());
-                tsq.valueDetect();
-                collector.putDouble(rowWindow.getRow(0).getTime(), tsq.getValidity());
-            }
-        } catch (IOException | NoNumberException ex) {
-            Logger.getLogger(UDTFCompleteness.class.getName()).log(Level.SEVERE, null, ex);
+    public void transform(Row row, PointCollector collector) throws Exception {
+        double value = Util.getValueAsDouble(row);
+        long time = row.getTime();
+        if (Double.isFinite(value)) {
+            tsq.updateAll(time, value);
         }
+    }
+
+    @Override
+    public void terminate(PointCollector collector) throws Exception {
+        tsq.updateDPAll(usePreSpeed, sMin, sMax);
+        tsq.updateReverseDPAll(usePreSpeed, sMin, sMax);
+        collector.putDouble(0, tsq.getValidity());
     }
 
 }
